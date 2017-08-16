f29fc514dfa333a09882750dc8aeeafce349e6ac
Darryl Tarr <tarrdog@gmail.com>
2017-08-16 11:55:26 -0500

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :likes
+  has_many :likes, dependent: :destroy
   has_many :users_liked, through: :likes, source: :user
 
   validates :content, presence: true, length: { minimum: 10 }
